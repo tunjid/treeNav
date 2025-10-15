9d38647b146fcfd1b5c52dc42885fbb08250ffa0
Adetunji Dahunsi <tjdah100@gmail.com>
2025-10-15 17:39:52 -0500

    Keep Defaults object internal

--- library/compose/src/commonMain/kotlin/com/tunjid/treenav/compose/Defaults.kt
+++ library/compose/src/commonMain/kotlin/com/tunjid/treenav/compose/Defaults.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 
 @Stable
- object Defaults {
+ internal object Defaults {
 
     val EmptyElement: @Composable (Any?, Modifier) -> Unit = { _, _ -> }
 
