1a1b3a8ca69a5d2e1a17879ec64898bb8364f9db
Adetunji Dahunsi <tjdah100@gmail.com>
2026-04-10 09:45:04 -0400

    Update iOS app content view

--- sample/iosApp/treeNav/ContentView.swift
+++ sample/iosApp/treeNav/ContentView.swift
@@ -10,16 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            ComposeView()
-                // Compose has own keyboard handler
-                .ignoresSafeArea(edges: .bottom)
-        }
-        .padding()
+        ComposeView()
+            .ignoresSafeArea()
     }
 }
 
